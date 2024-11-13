cb1bf804d3b939f2d0bc08b29dee56c02c7d3de1
Kashan Asim <kashan.asim@sigma-emea.com>
2024-11-13 13:34:13 +0200

    small promo added

---
+++ main.js
@@ -0,0 +1,72 @@
+globalThis.cv = Object.assign(globalThis.cv || {}, {
+  "general": {
+    "profilePhoto": "https://firebasestorage.googleapis.com/v0/b/maitake-project.appspot.com/o/profilePhotos%2FOYUjP3jHkhc1Rf5XPPHsF2gpAFE2%2F22aad3fa-5770-4b90-b9f9-48aa7a44015f-sri.jpg?alt=media&token=6db3e84f-f3e7-445f-b904-1080a3191dee",
+    "username": "sri",
+    "displayName": "Kashan Asim",
+    "profession": "Senior Software Engineer",
+    "location": "PK",
+    "pronouns": "He/him",
+    "byline": "Senior Software Engineer",
+    "website": "https://kashanasim.netlify.app",
+    "websiteURL": "https://kashanasim.netlify.app",
+    "about": null,
+    "status": {
+      "text": "\n",
+      "emoji": "🙃",
+      "timestamp": "2021-01-04T22:08:04.906Z"
+    }
+  },
+  "projects": [],
+  "sideProjects": [],
+  "exhibitions": [],
+  "talks": [],
+  "writing": [],
+  "awards": [],
+  "features": [],
+  "workExperience": [],
+  "volunteering": [],
+  "education": [],
+  "certifications": [],
+  "contact": [
+    {
+      "id": "3KhrZn2inhSNIz1LVxvq",
+      "platform": "LinkedIn",
+      "handle": "Kashan Asim",
+      "url": "https://www.linkedin.com/in/k%C4%81sh%C4%81n-asim-7a813a174"
+    },
+    {
+      "id": "a9mcCX9vUMCOrCJtnmw3",
+      "platform": "Open for: SWE roles",
+      "handle": "linkedin",
+      "url": "https://www.linkedin.com/in/k%C4%81sh%C4%81n-asim-7a813a174/"
+    },
+    {
+      "id": "a9mcCX9vUMCadCJtnmw3",
+      "platform": "My CV",
+      "handle": "personal site",
+      "url": "https://kashanasim.netlify.app"
+    }
+  ],
+  "writingTab": {
+    "pages": []
+  },
+  "allCollections": [
+    {
+      "name": "Contact",
+      "items": [
+        {
+          "id": "3KhrZn2inhSNIz1LVxvq",
+          "platform": "LinkedIn",
+          "handle": "Kashan Asim",
+          "url": "https://www.linkedin.com/in/k%C4%81sh%C4%81n-asim-7a813a174/"
+        },
+        {
+          "id": "a9mcCX9vUMCOrCJtnmw3",
+          "platform": "Open for: SWE roles",
+          "handle": "linkedin",
+          "url": "https://www.linkedin.com/in/k%C4%81sh%C4%81n-asim-7a813a174/"
+        }
+      ]
+    }
+  ]
+});
